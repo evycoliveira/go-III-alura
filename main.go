c665f4238deb75ae77e919b0492cc7e9dbb00657
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-02-06 12:32:58 -0300

    Add struct and inserting to the template

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 // Variável para carregar todos os templates que seja no formato HTML
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -14,7 +21,14 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 }
 
-// Função para
+// Função para executar os templates
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
